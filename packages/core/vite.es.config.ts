f4458a8a51aa5559cba0403654307d9eeb7509b9
GDCzhou <996078450@qq.com>
2024-06-23 22:06:39 +0800

    分包

--- packages/core/vite.es.config.ts
+++ packages/core/vite.es.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
+import { readdirSync } from 'fs';
+import { defer, delay, filter, map, includes } from "lodash-es";
+
+function getDirectoriesSync(basePath: string) {
+  const entries = readdirSync(basePath, { withFileTypes: true });
+
+  return map(
+    filter(entries, (entry) => entry.isDirectory()),
+    (entry) => entry.name
+  );
+}
+
 
 export default defineConfig({
   plugins: [vue(),dts({
@@ -33,6 +45,22 @@ export default defineConfig({
             }
             return chunkInfo.name as string
         },
+        // 手动分包
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+          if (id.includes('/packages/hooks')) {
+            return 'hooks'
+          }
+          if (id.includes('/packages/utils')) {
+            return 'utils'
+          }
+
+          for (const item of getDirectoriesSync("../components")) {
+            if (includes(id, `/packages/components/${item}`)) return item;
+          }
+        }
       }
     }
   }
