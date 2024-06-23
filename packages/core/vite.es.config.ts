70977a7ff9aa9d478ca9df2a52fee78f74281b86
GDCzhou <996078450@qq.com>
2024-06-23 21:27:06 +0800

    dts生成types文件

--- packages/core/vite.es.config.ts
+++ packages/core/vite.es.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),dts({
+    tsconfigPath: '../../tsconfig.build.json',
+    outDir: 'dist/types'
+  })],
   build: {
     outDir: 'dist/es',
     lib: {
